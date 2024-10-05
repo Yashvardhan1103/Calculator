3626e82b5d76b0d8d070d8be3b6d2670e25d3abb
Yashvardhan1103 <yashvardhan.sh@gmail.com>
2024-10-05 15:16:17 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,59 @@
+n = 0
+temp = 0
+while True:
+    if n == 0:
+        a = float(input('Enter first operand: '))
+        x = input('Enter operation: ')
+        b = float(input('Enter second operand: '))
+        if(x == '+'):
+            n = a + b
+            print(a, '+', b, '=', n)
+        elif(x == '-'):
+            n = a - b
+            print(a, '-', b, '=', n)
+        elif(x == '*'):
+            n = a * b
+            print(a, '*', b, '=', n)
+        elif(x == '/'):
+            n = a / b
+            print(a, '/', b, '=', n)
+        elif(x == '//'):
+            n = a // b
+            print(a, '//', b, '=', n)
+        else:
+            print('Incorrect operator, restart!')
+            continue
+    else:
+        y = input('Enter operation: ')
+        if(y in ['+', '-', '*', '/', '//']):
+            c = float(input('Enter operand: '))
+            if(y == '+'):
+                n1 = n
+                n = n + c
+                print(n1, '+', c, '=', n)
+            elif(y == '-'):
+                n1 = n
+                n = n - c
+                print(n1, '-', c, '=', n)
+            elif(y == '*'):
+                n1 = n
+                n = n * c
+                print(n1, '*', c, '=', n)
+            elif(y == '/'):
+                n1 = n
+                n = n / c
+                print(n1, '/', c, '=', n)
+            elif(y == '//'):
+                n1 = n
+                n = n // c
+                print(n1, '//', c, '=', n)
+        elif(y == 'End'):
+            temp = 1
+        else:
+            print('Incorrect operator, try again!')
+    if(temp == 1):
+        print('Result =', n)
+        break
+    else:
+        continue
+        
